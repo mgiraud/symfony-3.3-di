9da9025b2d12e0033602a0c93d59f4fddd6d7326
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2018-02-15 10:11:40 +0100

    Add files for default no di

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,19 +2,18 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Manager\DefaultManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request, TranslatorInterface $translator)
+    public function indexAction(Request $request)
     {
+        $translator = $this->get('translator');
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'salut' => $translator->trans('salut'),
@@ -25,8 +24,10 @@ class DefaultController extends Controller
     /**
      * @Route("/reverse", name="reverse")
      */
-    public function reverseAction(Request $request, TranslatorInterface $translator, DefaultManager $manager)
+    public function reverseAction(Request $request)
     {
+        $translator = $this->get('translator');
+        $manager = $this->get('manager.default');
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'salut' => $translator->trans($manager->reverseString('salut')),
