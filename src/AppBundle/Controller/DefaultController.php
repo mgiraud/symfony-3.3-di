adf37902c87420e8c606082883f9026646d5b136
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2018-02-16 15:23:30 +0100

    remove useless var

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -31,7 +31,6 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'salut' => $translator->trans($manager->reverseString('salut')),
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
 }
